868628dfe9cf8bdc23bc29f1f1303336535c3ed1
Samikate <samikate@hotmail.fr>
2023-01-18 12:05:08 +0100

    challenge Node04 finished

---
+++ server.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+// console.log(`The server will run on port ${process.env.SERVER_PORT}`);
+// console.log(`The server will connect to the database with the user "${process.env.DB_USER}" and the password "${process.env.DB_PASSWORD}`);
+console.log(`I am ${process.env.NAME}, Wilder in ${process.env.CITY}, and I love ${process.env.LANGUAGE}. `)
